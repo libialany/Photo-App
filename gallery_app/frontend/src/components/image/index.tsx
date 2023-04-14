a5c830ad5f3aa7609ab26076955203c9b7a07bb7
melany estevez camata <elibialany@gmail.com>
2023-04-14 05:08:11 -0400

    task added

---
+++ gallery_app/frontend/src/components/image/index.tsx
@@ -0,0 +1,47 @@
+
+
+
+import React, { useState } from "react";
+
+import { Backdrop, Fade, Modal } from "@mui/material";
+
+
+
+interface ModalProps {
+    open: boolean;
+    CloseModal: any;
+    image: string
+}
+
+export default function ImageModal({ open, CloseModal, image }: ModalProps) {
+    const handleClose = () => CloseModal(false);
+    return (
+
+        <Modal
+            sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                "&:hover": {
+                    backgroundcolor: "red"
+                }
+            }}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500
+            }}
+        >
+            <Fade in={open} timeout={500}>
+                <img
+                    src={image}
+                    alt="asd"
+                    style={{ maxHeight: "90%", maxWidth: "90%" }}
+                />
+            </Fade>
+        </Modal>
+
+    );
+}
---
+++ gallery_app/frontend/src/pages/album/index.tsx
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SignIn from '@/components/sign-in/SignIn';
+import ImageModal from '@/components/image';
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  const [open, setOpen] = useState<boolean>(false)
+  const [openImagen, setOpenImagen] = useState<boolean>(false)
+  const [image, setImage] = useState<string>("")
+  return (
+    <>
+      <SignIn open={open} CloseModal={setOpen} />
+      <ImageModal open={openImagen} CloseModal={setOpenImagen} image={image} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <CameraIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" color="inherit" noWrap>
+              Album App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Album App
+              </Typography>
+              <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                Something short and leading about the collection below—its contents,
+                the creator, etc. Make it short and sweet, but not too short so folks
+                don&apos;t simply skip over it entirely.
+              </Typography>
+              <Stack
+                sx={{ pt: 4 }}
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+              >
+                <Button variant="contained" onClick={() => {
+                  setOpen(true)
+                }}>Sign In</Button>
+              </Stack>
+            </Container>
+          </Box>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: '56.25%',
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to describe the
+                        content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" onClick={() => {
+                        setImage("https://source.unsplash.com/random")
+                        setOpenImagen(true)
+                      }}>View</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+        </Box>
+        {/* End footer */}
+      </ThemeProvider>
+
+    </>);
+}
