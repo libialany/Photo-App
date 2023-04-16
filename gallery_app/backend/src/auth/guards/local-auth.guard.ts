57866aa6a1dbd038c6d6ab5ec35b3fa4eb8b1567
melany estevez camata <elibialany@gmail.com>
2023-04-16 17:27:30 -0400

    ch: adding Guards

--- gallery_app/backend/src/auth/guards/local-auth.guard.ts
+++ gallery_app/backend/src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,26 @@
-import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const {
+      originalUrl,
+      query,
+      route,
+      method: action,
+    } = context.switchToHttp().getRequest();
+    const resource = Object.keys(query).length ? route.path : originalUrl;
+
+    try {
+      const isPermitted = (await super.canActivate(context)) as boolean;
+      if (!isPermitted) throw new UnauthorizedException();
+    } catch (err) {
+      throw err;
+    }
+    return true;
+  }
+}
