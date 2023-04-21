663d53bbebd8f9893ee378eda032351d671c1b3b
libialany <elibialany@gmail.com>
2023-04-21 17:38:33 -0400

    feat: added next.config.js , .env vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: { NEXT_PUBLIC_BASE_URL: "http://localhost:5000" },
+  env: {
+    SRC_IMAGES:
+      "",
+    DST_IMAGES: "/images",
+    NEXT_PUBLIC_BASE_URL: "http://localhost:5000",
+    NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
+  },
 };
 
 module.exports = nextConfig;
