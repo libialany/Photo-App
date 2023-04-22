8ae2157a1c59ee116fa130c7f9fcd1a0f10ef870
melany estevez camata <elibialany@gmail.com>
2023-04-22 19:53:59 -0400

    fix: reload view img

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     SRC_IMAGES:
-      "",//SRC_IMAGES=[path-for-save-images-backend-frontend]
+      "",// SRC_IMAGES=[path-for-save-images-backend-frontend]
     DST_IMAGES: "/images",
     NEXT_PUBLIC_BASE_URL: "http://localhost:5000",
     NEXT_PUBLIC_FRONTEND_URL: "http://localhost:3000",
